e51e60e3593cde5a0224dc2d4f9f3bc8e015838e
Adexandria <adeolaaderibigbe09@gmail.com>
2026-05-08 20:11:18 +0200

    Update the json naming

--- FoodLLMService/validations/request.py
+++ FoodLLMService/validations/request.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, Field
 
 class PredictionLabel(BaseModel):
-    Specific_dish: str = Field(..., description="Whether the user wants specific dishes or not")
-    Cuisine: str = Field(..., description="The type of cuisine the user is interested in")
-    Parent_category: str = Field(..., description="The parent dish or category the user is interested in")
-    Food_group: list[str] = Field(..., description="The food group the user is interested in")
+    specific_dish: str = Field(..., description="Whether the user wants specific dishes or not")
+    cuisine: str = Field(..., description="The type of cuisine the user is interested in")
+    parent_category: str = Field(..., description="The parent dish or category the user is interested in")
+    food_group: list[str] = Field(..., description="The food group the user is interested in")
 
 class PredictionResponse(BaseModel):
     predicted_label: PredictionLabel = Field(..., description="The predicted label for the food item")
